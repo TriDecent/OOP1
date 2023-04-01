ff087c648f0da1d5dd4f0f671c4ba7f6469930a6
TriDecent <116976929+TriDecent@users.noreply.github.com>
2023-04-01 15:39:20 +0700

    Add files via upload

---
+++ Bai04.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b)
+{
+  int r;
+  while (b)
+  {
+      r = a%b;
+      a=b;
+      b=r;
+  }
+  return a;
+}
+
+struct PhanSo
+{
+    int iTuSo;
+    int iMauSo;
+
+};
+
+void NhapPS(PhanSo &p)
+{
+    cout << "Nhap tu so: ";
+    cin >> p.iTuSo;
+    cout << "Nhap mau so: ";
+    cin >> p.iMauSo;
+    cout << endl;
+    while (p.iMauSo == 0)
+    {
+        cout << "Phan so khong hop le, hay nhap lai: ";
+        cin >> p.iMauSo;
+    }
+}
+
+void RutGonPS(PhanSo &x)
+{
+    int ucln = gcd(x.iTuSo, x.iMauSo);
+
+    if (x.iMauSo == gcd(x.iTuSo, x.iMauSo))
+        x.iTuSo = x.iTuSo/x.iMauSo;
+    else
+         x.iTuSo = x.iTuSo/ucln;
+         x.iMauSo = x.iMauSo/ucln;
+
+    if (x.iMauSo < 0 && x.iTuSo > 0)
+    {
+        x.iTuSo = -x.iTuSo;
+        x.iMauSo = -x.iMauSo;
+    }
+}
+bool SoSanhPS1(PhanSo a, PhanSo b)
+{
+    return a.iTuSo*b.iMauSo < b.iTuSo*a.iMauSo;
+}
+bool SoSanhPS2(PhanSo a, PhanSo b)
+{
+    return a.iTuSo*b.iMauSo > b.iTuSo*a.iMauSo;
+}
+void XuatPS(PhanSo x)
+{
+   int ucln = gcd(x.iTuSo, x.iMauSo);
+    if (x.iMauSo)
+    {
+        if (x.iMauSo == ucln)
+             cout << x.iTuSo << " ";
+        else
+             cout <<  x.iTuSo << "/" << x.iMauSo << " ";
+    }
+}
+PhanSo TimPhanSoMax(PhanSo a[], int n)
+{
+    PhanSo kq = *max_element(a, a+n, SoSanhPS1);
+    return kq;
+}
+void SapXepPhanSoTangDan(PhanSo a[], int n)
+{
+    sort(a,a+n,SoSanhPS1);
+}
+void SapXepPhanSoGiamDan(PhanSo a[], int n)
+{
+    sort(a,a+n,SoSanhPS2);
+}
+int main()
+{
+   int n;
+   cout << "Nhap so luong phan so: ";
+   cin >> n;
+   cout << endl;
+
+   PhanSo *a = new PhanSo[n];
+   for(int i = 0; i < n; ++i)
+   {
+       cout << "Nhap phan so thu " << i + 1 << endl;
+       NhapPS(a[i]);
+       RutGonPS(a[i]);
+   }
+   cout << "Phan so lon nhat tim duoc la: ";
+   XuatPS(TimPhanSoMax(a,n));
+   cout << endl;
+
+   SapXepPhanSoTangDan(a,n);
+   cout << "Phan so sau khi duoc sap xep tang dan: ";
+   for(int i = 0; i < n; ++i)
+   {
+       XuatPS(a[i]);
+   }
+   cout << endl;
+
+   cout << "Phan so sau khi duoc sap xep giam dan: ";
+   SapXepPhanSoGiamDan(a,n);
+   for(int i = 0; i < n; ++i)
+   {
+       XuatPS(a[i]);
+   }
+   cout << endl;
+   delete []a;
+}
